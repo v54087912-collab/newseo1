ddc86f8f9496010ccd9a2f182750f879b99eb91e
v54087912-collab <v54087912@gmail.com>
2025-12-12 18:45:40 +0530

    Add files via upload

---
+++ frontend/next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // Rewrites section ko puri tarah hata diya gaya hai
+};
+
+export default nextConfig;
